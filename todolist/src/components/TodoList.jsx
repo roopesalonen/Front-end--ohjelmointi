eb9f9303fc811301138dba6d5fe3464613e183e3
Roope <roopejuuz@gmail.com>
2024-09-30 22:57:57 +0300

    Todolist

---
+++ todolist/src/components/TodoList.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function TodoList() {
+
+    const [todo, setTodo] = useState({ desc: "", date: "" });
+    const [todos, setTodos] = useState([]);
+
+    const handleChange = (e) => {
+        setTodo({ ...todo, [e.target.name]: e.target.value });
+    };
+
+    const addTodo = () => {
+        setTodos([...todos, todo]);
+    };
+
+    return (
+        <>
+            <input name="desc" placeholder="Description" onChange={handleChange} value={todo.desc} />
+            <input name="date" placeholder="Date" onChange={handleChange} value={todo.date} />
+            <button onClick={addTodo}>Add</button>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Date</th>
+                        <th>Description</th>
+                    </tr>
+                    {todos.map((todo, index) => (
+                        <tr key={index}>
+                            <td>{todo.date}</td>
+                            <td>{todo.desc}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
+}
