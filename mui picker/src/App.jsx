accea3cec75aa8cbce0b8b14182886181a2702a9
Roope <roopejuuz@gmail.com>
2024-10-07 23:49:14 +0300

    tab menu

--- mui picker/src/App.jsx
+++ mui picker/src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
-import TodoList from "./components/TodoList"
+import Tablist from "./components/Tablist";
 
 export default function App() {
   return (
     <Container maxWidth="xl">
       <CssBaseline />
-      <TodoList />
+      <Tablist />
     </Container>
   );
 }
