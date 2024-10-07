a0ef0bb3486bc1b506cd5b5553692673b69fe944
Roope <roopejuuz@gmail.com>
2024-10-07 23:50:49 +0300

    Revert "tab menu"
    This reverts commit accea3cec75aa8cbce0b8b14182886181a2702a9.

--- mui picker/src/App.jsx
+++ mui picker/src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
-import Tablist from "./components/Tablist";
+import TodoList from "./components/TodoList"
 
 export default function App() {
   return (
     <Container maxWidth="xl">
       <CssBaseline />
-      <Tablist />
+      <TodoList />
     </Container>
   );
 }
